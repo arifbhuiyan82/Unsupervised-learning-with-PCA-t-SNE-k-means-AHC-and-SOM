82d6250e5a961f5015fdd5c0413d4af3cbac7cf8
Arif Bhuiyan <arif_fnbd@yahoo.com>
2023-12-17 13:56:05 +0100

    update AHC for dataset2

--- Code for Dataset2/AHC.py
+++ Code for Dataset2/AHC.py
@@ -7,34 +7,29 @@ from sklearn.cluster import AgglomerativeClustering
 # Load training data
 filename = 'students- knowledge-level-data-numeric.txt'
 df = pd.read_csv(filename, delimiter=',', header=0)
-
-# Print the loaded dataset
 print(df)
 
-# Define the feature columns and target column
-features = ['STG', 'SCG', 'STR', 'LPR', 'PEG']
+features = ['STG','SCG','STR','LPR','PEG']
 target = ['UNS']
 
-# Separating out the features from the dataset
+# Separating out the features
 X = df.loc[:, features].values
 
-# Define a function to plot a dendrogram from a hierarchical clustering model
 def plot_dendrogram(model, **kwargs):
     # Create linkage matrix and then plot the dendrogram
 
-    # Create an array to store the counts of samples under each node
+    # create the counts of samples under each node
     counts = np.zeros(model.children_.shape[0])
     n_samples = len(model.labels_)
     for i, merge in enumerate(model.children_):
         current_count = 0
         for child_idx in merge:
             if child_idx < n_samples:
-                current_count += 1  # Leaf node
+                current_count += 1  # leaf node
             else:
                 current_count += counts[child_idx - n_samples]
         counts[i] = current_count
 
-    # Create a linkage matrix
     linkage_matrix = np.column_stack(
         [model.children_, model.distances_, counts]
     ).astype(float)
@@ -42,21 +37,24 @@ def plot_dendrogram(model, **kwargs):
     # Plot the corresponding dendrogram
     dendrogram(linkage_matrix, **kwargs)
 
-# Create an Agglomerative Clustering model with average linkage
-# and specify the distance metric using the 'metric' parameter
-model = AgglomerativeClustering(metric='euclidean', linkage='average', distance_threshold=0, n_clusters=None)
+# ‘average’ uses the average of the distances of each observation of the two sets.
+# setting distance_threshold=0 ensures we compute the full tree.
+model = AgglomerativeClustering(affinity='euclidean', linkage='average', distance_threshold=0, n_clusters=None)
 
-# Fit the model to the feature data
 model = model.fit(X)
-
-# Plot the dendrogram with a title
 plt.title("Hierarchical Clustering Dendrogram using average linkage")
-
-# Plot the top three levels of the dendrogram
+# plot the top three levels of the dendrogram
 plot_dendrogram(model, truncate_mode="level", p=3)
-
-# Add labels to the plot
 plt.xlabel("Number of points in node (or index of point if no parenthesis).")
+plt.show()
 
-# Show the dendrogram plot
+# ‘complete’ linkage uses the maximum distances between all observations of the two sets.
+# setting distance_threshold=0 ensures we compute the full tree.
+model = AgglomerativeClustering(affinity='euclidean', linkage='complete', distance_threshold=0, n_clusters=None)
+
+model = model.fit(X)
+plt.title("Hierarchical Clustering Dendrogram using complete linkage")
+# plot the top three levels of the dendrogram
+plot_dendrogram(model, truncate_mode="level", p=3)
+plt.xlabel("Number of points in node (or index of point if no parenthesis).")
 plt.show()
